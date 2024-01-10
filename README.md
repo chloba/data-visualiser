7ab9333162345f7cc6e9d7c29751c169203d0ccf
chloba <chloba@noc.ac.uk>
2024-01-10 15:58:49 +0000

    feat: get data

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # Covid 19 Data Visualiser App - Tutorial by Diligent Dev
 
 ## Project setup
-```
+
 yarn install
-```
 
 ### Compiles and hot-reloads for development
-```
+
 yarn serve
-```
 
 ### Compiles and minifies for production
-```
+
 yarn build
-```
 
 ### Lints and fixes files
-```
+
 yarn lint
-```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import moment from "moment";
+// import LineChart from "./components/LineChart.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // LineChart,
+  },
+  data() {
+    return {
+      arrPositive: [],
+      arrHospitalised: [],
+      arrInIcu: [],
+      arrOnVentilators: [],
+      arrRecovered: [],
+      arrDeaths: [],
+    };
+  },
+  async created() {
+    const { data } = await axios.get(
+      "https://api.covidtracking.com/v1/us/daily.json"
+    );
+    data.forEach((d) => {
+      const date = moment(d.date, "YYYYMMDD").format("DD/MM");
+      const {
+        positive,
+        hospitalizedCurrently,
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death,
+      } = d;
+      this.arrPositive.push({ date, total: positive });
+      this.arrHospitalised.push({ date, total: hospitalizedCurrently });
+      this.arrInIcu.push({ date, total: inIcuCurrently });
+      this.arrOnVentilators.push({ date, total: onVentilatorCurrently });
+      this.arrRecovered.push({ date, total: recovered });
+      this.arrDeaths.push({ date, total: death });
+    });
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
